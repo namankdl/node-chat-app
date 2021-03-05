12897ceac90d4c76b39bc77037901cc843c5c77f
namanjainkdl <namanjainkdl@gmail.com>
2021-03-05 20:04:25 +0530

    Add Send Location Button

--- public/js/index.js
+++ public/js/index.js
@@ -18,6 +18,17 @@ socket.on('newMessage', function (message) {
     $('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function(message){
+    var li = $('<li></li>');
+    var a = $('<a target="_blank">My current Location</a>');
+
+    li.text(`${message.from}:`);
+    a.attr('href', message.url);
+    li.append(a);
+
+    $('#messages').append(li);
+});
+
 
 $('#message-form').on('submit', function(e) {
     e.preventDefault();
@@ -27,5 +38,24 @@ $('#message-form').on('submit', function(e) {
         text: $('[name = message]').val()
     }, function() {
 
+    });
+});
+
+var locationButton = $('#send-location');
+locationButton.on('click',function (){
+    if (!navigator.geolocation)
+    {
+        return alert('Geolocation is not supported by your browser');
+    }
+
+    navigator.geolocation.getCurrentPosition(function(position){
+    socket.emit('createLocationMessage',{
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+    });
+    console.log('hello',position);
+
+    },function() {
+        alert('Unable to fetch the location');
     });
 });
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-const {generateMessage} = require('./utils/message');
+const {generateMessage,generateLocationMessage} = require('./utils/message');
 
 const publicPath = path.join(__dirname, '/../public');
 const port = process.env.PORT || 8090;
@@ -38,15 +38,13 @@ io.on('connection', (socket) => {
         //when a user send any data it can be broadcast to everyone.
         io.emit('newMessage',generateMessage(message.from,message.text));
 
-        
+
         callback('This is from the server');
+    });
 
-        // socket.broadcast.emit('newMessage',{ 
-        //     from:message.from,
-        //     text:message.text,
-        //     createdAt: new Date().getTime()
-        // });
-
+    socket.on('createLocationMessage', (coords) => { 
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+        console.log(coords.latitude);
     });
 
     socket.on('disconnect', () => {
